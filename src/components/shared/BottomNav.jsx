136a7ab9c2ac379cb1fd62702dd3770a5a843599
AliAbdurohman16 <alyabdurohman16@gmail.com>
2025-08-13 12:58:06 +0700

    add: temporary data with redux

--- src/components/shared/BottomNav.jsx
+++ src/components/shared/BottomNav.jsx
@@ -3,16 +3,22 @@ import { BiSolidDish } from 'react-icons/bi';
 import { CiCircleMore } from 'react-icons/ci';
 import { FaHome } from 'react-icons/fa';
 import { MdOutlineReorder, MdTableBar } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Modal from './Modal';
+import { useDispatch } from 'react-redux';
+import { setCustomer } from '../../redux/slices/customerSlice';
 
 const BottomNav = () => {
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const dispatch = useDispatch();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [guestCount, setGuestCount] = useState(0);
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
+    const [name, setName] = useState();
+    const [phone, setPhone] = useState();
     
     const increment = () => {
         if (guestCount >= 6) return;
@@ -24,20 +30,34 @@ const BottomNav = () => {
         setGuestCount((prev) => prev - 1);
     };
 
+    const isActive = (path) => location.pathname === path;
+
+    const handleCreateOrder = () => {
+        // send the data to store
+        dispatch(setCustomer({ name, phone, guests: guestCount }));
+        navigate("/tables");
+    }
+
     return (
         <div className="fixed bottom-0 left-0 right-0 bg-[#262626] flex justify-around p-2 h-16">
-            <button onClick={() => navigate("/")} className="flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]"><FaHome className="inline mr-2" size={20} /> <p>Home</p></button>
-            <button onClick={() => navigate("/orders")} className="flex items-center justify-center text-[#ababab] w-[200px]"><MdOutlineReorder className="inline mr-2" size={20} /> <p>Orders</p></button> 
-            <button onClick={() => navigate("/tables")} className="flex items-center justify-center text-[#ababab] w-[200px]"><MdTableBar className="inline mr-2" size={20} /> <p>Tables</p></button>
+            <button onClick={() => navigate("/")} className={`flex items-center justify-center font-bold ${isActive("/") ? "text-[#f5f5f5] bg-[#343434]": "text-[#ababab]"} w-[200px] rounded-[20px]`}><FaHome className="inline mr-2" size={20} /> <p>Home</p></button>
+            <button onClick={() => navigate("/orders")} className={`flex items-center justify-center font-bold ${isActive("/orders") ? "text-[#f5f5f5] bg-[#343434]": "text-[#ababab]"} w-[200px] rounded-[20px]`}><MdOutlineReorder className="inline mr-2" size={20} /> <p>Orders</p></button> 
+            <button onClick={() => navigate("/tables")} className={`flex items-center justify-center font-bold ${isActive("/tables") ? "text-[#f5f5f5] bg-[#343434]": "text-[#ababab]"} w-[200px] rounded-[20px]`}><MdTableBar className="inline mr-2" size={20} /> <p>Tables</p></button>
             <button className="flex items-center justify-center text-[#ababab] w-[200px]"><CiCircleMore className="inline mr-2" size={20} /> <p>More</p></button>
 
-            <button onClick={openModal} className="absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] rounded-full p-3 items-center"><BiSolidDish size={30} /></button>
+            <button disabled={isActive("/tables") || isActive("/menu")} onClick={openModal} className="absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] rounded-full p-3 items-center"><BiSolidDish size={30} /></button>
 
             <Modal isOpen={isModalOpen} onClose={closeModal} title="Create Order">
                 <div>
                     <label className="block text-[#ababab] mb-2 text-sm font-medium">Customer Name</label>
                     <div className="flex items-center rounded-lg py-3 px-4 bg-[#1f1f1f]">
-                        <input type="text" name="" placeholder="Enter customer name" id="" className="bg-transparent flex-1 text-white focus:outline-none" />
+                        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter customer name" id="" className="bg-transparent flex-1 text-white focus:outline-none" />
+                    </div>
+                </div>
+                <div>
+                    <label className="block text-[#ababab] mb-2 text-sm font-medium mt-3">Customer Phone</label>
+                    <div className="flex items-center rounded-lg py-3 px-4 bg-[#1f1f1f]">
+                        <input type="number" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="08234567890" id="" className="bg-transparent flex-1 text-white focus:outline-none" />
                     </div>
                 </div>
                 <div>
@@ -48,7 +68,7 @@ const BottomNav = () => {
                         <button onClick={increment} className="text-yellow-500 text-2xl">&#43;</button>
                     </div>
                 </div>
-                <button onClick={() => navigate("/tables")} className="w-full bg-[#f6b100] text-[#f5f5f5] rounded-lg py-3 mt-8 hover:bg-yellow-700">
+                <button onClick={handleCreateOrder} className="w-full bg-[#f6b100] text-[#f5f5f5] rounded-lg py-3 mt-8 hover:bg-yellow-700">
                     Create Order
                 </button>
             </Modal>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import BottomNav from "../components/shared/BottomNav";
 import BackButton from "../components/shared/BackButton";
 import { MdRestaurantMenu } from "react-icons/md";
@@ -6,8 +6,12 @@ import MenuContainer from "../components/menu/MenuContainer";
 import CustomerInfo from "../components/menu/CustomerInfo";
 import CartInfo from "../components/menu/CartInfo";
 import Bill from "../components/menu/Bill";
+import { useSelector } from "react-redux";
 
 const Menu = () => {
+
+    const customerData = useSelector(state => state.customer);
+
     return (
         <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
             {/* Left Div */}
@@ -21,8 +25,8 @@ const Menu = () => {
                         <div className="flex items-center gap-3 cursor-pointer">
                             <MdRestaurantMenu className="text-[#f5f5f5] text-4xl" />
                             <div className="flex flex-col items-start">
-                                <h1 className="text-md text-[#f5f5f5] font-semibold">Customer Name</h1>
-                                <p className="text-xs text-[#ababab] font-medium">Table No: 2</p>
+                                <h1 className="text-md text-[#f5f5f5] font-semibold">{customerData.customerName  || "Customer Name"}</h1>
+                                <p className="text-xs text-[#ababab] font-medium">{customerData.tableNo  || "N/A"}</p>
                             </div>
                         </div>
                     </div>
